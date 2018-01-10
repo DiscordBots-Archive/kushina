8e82b7dbcbdd454373c86d3b45befb15f1a7d725
EsdeseEsdeath <35155224+EsdeseEsdeath@users.noreply.github.com>
2018-01-10 19:53:30 +0300

    Update channeltopic.js

--- cmds/channeltopic.js
+++ cmds/channeltopic.js
@@ -1,12 +1,13 @@
 const Discord = require("discord.js");
 module.exports.run = async (client, message, args) => {
+    const channel = message.channel;
     var embedcolor = '#' + ("000000" + Math.random().toString(16).slice(2, 8).toUpperCase()).slice(-6);
   const embed = new Discord.RichEmbed()
     .setColor(embedcolor)
-    .setTitle(`Channel Topic of ${message.channel.name}`)
-    .setDescription(message.channel.topic)
+    .setTitle(`Channel Topic of ${channel.name}`)
+    .setDescription(channel.topic)
     .setFooter("Powered by Uzumaki-Clan");
-  if (!message.channel.topic) embed.setDescription("No Topic set.");
+  if (!channel.topic) embed.setDescription("No Topic set.");
   message.channel.send({embed});
 }
 
