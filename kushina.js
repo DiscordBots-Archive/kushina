a32d8f27f483f3c2931f6820ca82812bc3b7aaee
EsdeseEsdeath <35155224+EsdeseEsdeath@users.noreply.github.com>
2018-01-10 17:54:30 +0300

    Update kushina.js

--- kushina.js
+++ kushina.js
@@ -21,7 +21,7 @@ fs.readdir("./cmds/", (err,files) => {
 })
 client.on("ready", async () => {
     console.log(`Kushina is online, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`);
-    client.user.setGame("-help // Neko World");
+    client.user.setGame("//help || Uzumaki Princess");
 });
 
 client.login(process.env.BOT_TOKEN);
