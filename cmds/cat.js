05e723663a78834e90c04870c682ae886de80259
EsdeseEsdeath <35155224+EsdeseEsdeath@users.noreply.github.com>
2018-01-10 19:39:17 +0300

    Update cat.js

--- cmds/cat.js
+++ cmds/cat.js
@@ -1,18 +1,6 @@
 const Discord = require("discord.js");
-const cooldown = new Set();
 module.exports.run = async (client, message, args) => {
-    if (cooldown.has(message.author.id && message.guild.id)) {
-        return message.reply('**[COOLDOWN]** Info: command has **1 Minutes** Cooldown!');
-    }0
-    cooldown.add(message.author.id && message.guild.id);
-
-    setTimeout(() => {
-        cooldown.delete(message.author.id && message.guild.id);
-    }, 60000);
     const fetch = require('snekfetch');
-    /*
-      Fetch data from the restful API
-    */
     fetch.get('https://random.cat/meow').then(cat => {
         const embed = new Discord.RichEmbed()
     .setImage(`${cat.body.file}`)
