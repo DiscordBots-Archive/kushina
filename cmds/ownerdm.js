b1e6802131dabe80952da6560ba7425e3d6ddc31
EsdeseEsdeath <35155224+EsdeseEsdeath@users.noreply.github.com>
2018-01-10 21:04:28 +0300

    Create ownerdm.js

---
+++ cmds/ownerdm.js
@@ -0,0 +1,20 @@
+const Discord = require("discord.js");
+module.exports.run = async (client, message, args) => {
+let sigh = message.content.split(" ").slice(1);
+let psymsg = sigh.join(" ");
+if (!psymsg) return message.reply("Please specify what I should report to my owner?");
+message.channel.send(`${message.author.username} I send your message to my owner`);
+var embedcolor = '#' + ("000000" + Math.random().toString(16).slice(2, 8).toUpperCase()).slice(-6);
+        const embed = new Discord.RichEmbed()
+        .setDescription(`${message.author.tag}'s report`)
+        .addField("Server", `${message.guild.name}`)
+        .setThumbnail(message.author.displayAvatarURL)
+        .addField("Message", psymsg)
+        .setColor(embedcolor)
+        .setFooter("Powered by Hanekawa.Bot™");
+client.users.get("366677235597574155").send({embed: embed});
+}
+
+module.exports.help = {
+    name: "ownerdm"
+}
