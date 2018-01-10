1fe6b71a93bb6df276184884eaa5ea7af8a70c27
EsdeseEsdeath <35155224+EsdeseEsdeath@users.noreply.github.com>
2018-01-10 21:49:17 +0300

    Create meme.js

---
+++ cmds/meme.js
@@ -0,0 +1,24 @@
+const Discord = require("discord.js");
+module.exports.run = async (client, message, args) => {
+    var sent = undefined
+    var request = require('request');
+    let msg = await message.channel.send("Image Loading...").then(function(msg) {
+        request('https://www.reddit.com/r/memes/random/.json', function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                var json = JSON.parse(body);
+                var embedcolor = '#' + ("000000" + Math.random().toString(16).slice(2, 8).toUpperCase()).slice(-6);
+                const embed = new Discord.RichEmbed()
+                .setDescription("Random memes. :joy: ")
+                .setColor(embedcolor)
+                .setImage(`${json[0].data.children[0].data.preview.images[0].source.url}`)
+                .setFooter("Powered by Uzumaki-Clan");
+                message.channel.send({embed: embed});
+                msg.delete();
+            }
+        });
+    });
+}
+
+module.exports.help = {
+    name: "meme"
+}
