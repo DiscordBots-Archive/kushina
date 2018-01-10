fdf438bf285b50334d09df82465d0a2eef5d50a7
EsdeseEsdeath <35155224+EsdeseEsdeath@users.noreply.github.com>
2018-01-10 21:09:23 +0300

    Create embed.js

---
+++ cmds/embed.js
@@ -0,0 +1,15 @@
+const Discord = require("discord.js");
+module.exports.run = async (client, message, args) => {
+    var embedcolor = '#' + ("000000" + Math.random().toString(16).slice(2, 8).toUpperCase()).slice(-6);
+    const embed = new Discord.RichEmbed()
+            .setAuthor(message.author.tag, message.author.displayAvatarURL)
+            .setThumbnail(message.author.displayAvatarURL)
+            .setColor(embedcolor)
+            .addField("Your text", args.join(' '), true)
+            .setFooter("Powered by Hanekawa.Bot™");
+            return message.channel.send({embed: embed});
+}
+
+module.exports.help = {
+    name: "embed"
+}
