0e513427921a60d3222d6fe5e7081a1f539f3f69
EsdeseEsdeath <35155224+EsdeseEsdeath@users.noreply.github.com>
2018-01-10 18:31:22 +0300

    Update ban.js

--- cmds/ban.js
+++ cmds/ban.js
@@ -8,25 +8,26 @@ module.exports.run = async (client, message, args) => {
 				var user = client.users.get(banee.id);
 				var guild = message.guild;
 				var reason = message.content.split(" ").splice(1).join(" ");
-				
+
 				if(reason == "")
 					var reason = "Not specified.";
-				
+
 				banned.ban(reason);
 
 				message.reply(banee + " has been successfullly banned.");
-				
+
 				var ban = new Discord.RichEmbed();
-					ban.setAuthor(user.username, user.avatarURL)
+				ban.setColor(0xFFB200)
+					.setAuthor(user.username, user.avatarURL)
 					.addField('Member Banned', `**:hammer: ${user.username}#${user.discriminator} (${user.id}) was banned from the server.**`)
 					.addField('Responsible Moderator', message.member.displayName)
 					.addField('Reason', reason)
 					.setFooter(`${guild.name} | ${guild.members.size} members`, `${guild.iconURL}`)
-					.setColor("#F04903");
+					.setColor("#F04903")
 
 				try {
 					var log = message.guild.channels.find('name', 'mod-logs');
-					message.channel.send({embed: ban});
+					message.chanenl.send({embed: ban});
 				} catch (e) {
 					message.channel.send({embed: ban});
 				}
@@ -36,8 +37,8 @@ module.exports.run = async (client, message, args) => {
 		} else {
 			message.reply(" you do not have permission to perform this action!");
     }
-  
-}
+  }
+
 
 module.exports.help = {
     name: "ban"
