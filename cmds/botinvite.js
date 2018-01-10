46d3963f84a096e38f96ab8d3c3a78c329f38c48
EsdeseEsdeath <35155224+EsdeseEsdeath@users.noreply.github.com>
2018-01-10 19:23:24 +0300

    Update botinvite.js

--- cmds/botinvite.js
+++ cmds/botinvite.js
@@ -5,9 +5,9 @@ module.exports.run = async (client, message, args) => {
             const embed = new Discord.RichEmbed()
             .setAuthor(client.user.username, client.user.displayAvatarURL)
             .setThumbnail(client.user.displayAvatarURL)
-			.setTitle(`Invite me your server and you will have fun!`)
-			.setURL(client.user.bot ? `https://discordapp.com/oauth2/authorize?client_id=400353151817089057&scope=bot&permissions=2146958591` : `http://lmgtfy.com/?q=Didn%27t+you+read+the+damn+text`)
-			.setFooter(`Note: I may be a private bot`)
+	    .setTitle(`Invite me your server and you will have fun!`)
+	    .setURL(client.user.bot ? `https://discordapp.com/oauth2/authorize?client_id=400660172961349643&scope=bot&permissions=2146958591` : `http://lmgtfy.com/?q=Didn%27t+you+read+the+damn+text`)
+	    .setFooter(`Note: I may be a private bot`)
             .setColor(embedcolor)
             message.channel.send({embed: embed});
 
