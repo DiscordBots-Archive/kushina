8c2cbb06d3d1cbc76bae329582d4438d6eebb00d
EsdeseEsdeath <35155224+EsdeseEsdeath@users.noreply.github.com>
2018-01-14 12:50:35 +0300

    Update kushina.js

--- kushina.js
+++ kushina.js
@@ -19,9 +19,24 @@ fs.readdir("./cmds/", (err,files) => {
         
     });
 })
-client.on("ready", async () => {
-    console.log(`Kushina is online, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`);
-    client.user.setGame(`//help - online in ${client.guilds.size} servers.`);
+client.on("ready", () => {
+  // This event will run if the bot starts, and logs in, successfully.
+  console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`); 
+  // Example of changing the bot's playing game to something useful. `client.user` is what the
+  // docs refer to as the "ClientUser".
+  client.user.setGame(`//help || on ${client.guilds.size} servers`);
+});
+
+client.on("guildCreate", guild => {
+  // This event triggers when the bot joins a guild.
+  console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
+  client.user.setGame(`//help || on ${client.guilds.size} servers`);
+});
+
+client.on("guildDelete", guild => {
+  // this event triggers when the bot is removed from a guild.
+  console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
+  client.user.setGame(`//help || on ${client.guilds.size} servers`);
 });
 
 client.login(process.env.BOT_TOKEN);
