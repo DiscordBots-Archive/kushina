7a39dfadff1be72e6f97c26328a6e27aa9e876c7
EsdeseEsdeath <35155224+EsdeseEsdeath@users.noreply.github.com>
2018-01-16 15:21:07 +0300

    Update greet.js

--- cmds/greet.js
+++ cmds/greet.js
@@ -17,7 +17,14 @@ module.exports.run = async (client, message, args) => {
 		"Hewwo!",
 		"HERRO!!"
 	];
-	message.channel.send('' + message.author.toString() + ' umm.. ' + responses[Math.floor(Math.random() * responses.length)])
+	var embedcolor = '#' + ("000000" + Math.random().toString(16).slice(2, 8).toUpperCase()).slice(-6);
+	const embed = new Discord.RichEmbed()
+	.setDescription('Random Greetings!')
+	.setThumbnail(client.user.displayAvatarURL)
+	.addField("Greetings!", '' + message.author.toString() + ' umm.. ' + responses[Math.floor(Math.random() * responses.length)])
+        .setColor(embedcolor)
+	.setFooter("Powered by Uzumaki-clan")
+	message.channel.send({embed: embed})
 		
 }
 
