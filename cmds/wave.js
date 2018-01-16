790058d3dcced06d35b0373d7fa260cea41cf2a6
EsdeseEsdeath <35155224+EsdeseEsdeath@users.noreply.github.com>
2018-01-16 14:49:38 +0300

    Update wave.js

--- cmds/wave.js
+++ cmds/wave.js
@@ -20,7 +20,7 @@ module.exports.run = async (client, message, args) => {
     var embedcolor = '#' + ("000000" + Math.random().toString(16).slice(2, 8).toUpperCase()).slice(-6);
     if (message.mentions.users.first()) {
         const embed = new Discord.RichEmbed()
-        .setDescription(''+ message.mentions.users.first() + ' waves at '+ message.author.toString() + ' goodbye!')
+        .setDescription(''+ message.author.toString() + ' : Greetings '+ message.mentions.users.first() + ' !!')
         .setColor(embedcolor)
         .setImage(`${imageArray[wave]}`)
         .setFooter("Powered by Uzumaki-clan")
@@ -28,7 +28,7 @@ module.exports.run = async (client, message, args) => {
     } else {
         if (!message.mentions.users.first()) {
             const embed = new Discord.RichEmbed()
-            .setDescription(''+ message.author.toString() +' waves at everyone bye!')
+            .setDescription(''+ message.author.toString() +' Hewwo!! :heart: :wave:')
             .setColor(embedcolor)
             .setFooter("Powered by Uzumaki-clan")
             .setImage(`${imageArray[wave]}`)
