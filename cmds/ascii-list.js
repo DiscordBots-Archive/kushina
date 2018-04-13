4889a0a5a0d01fc987ef2fb01cab157399858bab
EsdeseEsdeath <35155224+EsdeseEsdeath@users.noreply.github.com>
2018-04-13 18:40:36 +0300

    Update ascii-list.js

--- cmds/ascii-list.js
+++ cmds/ascii-list.js
@@ -4,7 +4,7 @@ const RichEmbed = require('discord.js').RichEmbed;
 module.exports.run = async (client, message, args) => {
   let embed = new Discord.RichEmbed()
         .addField('List', stripIndents`
-                **Ascii Font List:**\n3-d\n3x5`);
+                **Ascii Font List:**\n3-d\n3x5\n5lineoblique\nacrobatic\nalligator\nalligator2\nalphabet\navatar\nbanner\nbanner3-D\nbanner3\nbanner4\nbarbwire\nbasic\nbell\nbig\nbigchief\nbinary\nblock\nbubble\nbulbhead\ncalgphy2\ncaligraphy\ncatwalk\nchunky\ncoinstak\ncolossal\ncomputer\ncontessa\ncontrast\ncosmic\ncosmike\ncricket\ncursive\ncyberlarge\ncybermedium\ncybersmall\ndiamond\ndigital\ndoh\ndoom\ndotmatrix\ndrpepper\neftichess\neftifont\neftipiti\neftirobot\neftitalic\neftiwall\neftiwater\nepic\nfender\nfourtops\nfuzzy\ngoofy\ngothic\ngraffiti\nhollywood\ninvita\nisometric1\nisometric2\nisometric3\nisometric4\nitalic\nivrit\njazmine\njerusalem\nkatakana\nkban\nlarry3d\nlcd\nlean\nletters\nlinux\nlockergnome\nmadrid\nmarquee\nmaxfour\nmike\nmini\nmirror\nmnemonic\nmorse\nmoscow\nnancyj-fancy\nnancyj-underlined\nnancyj\nnipples\nntgreek\no8\nogre\npawp\npeaks\npebbles\npepper\npoison\npuffy\npyramid\nrelief\nrelief2\nrev\nroman\nrot13\nrounded\nrowancap\nrozzo\nrunic\nrunyc\nsblood\nscript\nserifcap\nshadow\nshort\nslant\nslide\nslscript\nsmall\nsmisome1\nsmkeyboard\nsmscript\nsmshadow\nsmslant\nsmtengwar\nspeed\nstampatello\nstandard\nstarwars\nstellar\nstop\nstraight\ntanja\ntengwar\nterm\nthick\nthin\nthreepoint\nticks\nticksslant\ntinker-toy\ntombstone\ntrek\ntsalagi\ntwopoint\nunivers\nusaflag\nwavy\nweird`);
                 message.channel.send({embed: embed});
 }
 
