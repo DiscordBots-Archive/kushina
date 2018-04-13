5559d9b4f9c8c5a73fa483aa8e1b38ae3f6f1f43
EsdeseEsdeath <35155224+EsdeseEsdeath@users.noreply.github.com>
2018-04-13 18:39:02 +0300

    Update ascii-list.js

--- cmds/ascii-list.js
+++ cmds/ascii-list.js
@@ -4,7 +4,7 @@ const RichEmbed = require('discord.js').RichEmbed;
 module.exports.run = async (client, message, args) => {
   let embed = new Discord.RichEmbed()
         .addField('List', stripIndents`
-                **Ascii Font List:**\n\3-d\n\3x5`);
+                **Ascii Font List:**\n3-d\n3x5`);
                 message.channel.send({embed: embed});
 }
 
